ed1892940d2ad13b2ab475ed34ff486eae02a6f8
tonmoysaha <tonmoysahaopi@gmail.com>
2019-05-29 18:48:15 +0600

    add new class

---
+++ src/com/hibernate/mapping/entity/Instructor.java
@@ -0,0 +1,86 @@
+package com.hibernate.mapping.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "first_name")
+	private String first_name;
+
+	@Column(name = "last_name")
+	private String last_name;
+
+	@Column(name = "email")
+	private String email;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "instructor_detail_id")
+	private InstructorDetailClass instructorDetailClass;
+
+	public Instructor() {
+		super();
+	}
+
+	public Instructor(String first_name, String last_name, String email) {
+		super();
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public InstructorDetailClass getInstructorDetailClass() {
+		return instructorDetailClass;
+	}
+
+	public void setInstructorDetailClass(InstructorDetailClass instructorDetailClass) {
+		this.instructorDetailClass = instructorDetailClass;
+	}
+
+}
