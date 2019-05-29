8338d677168389bdc9d8c804b046b237def3c1fd
tonmoysaha <tonmoysahaopi@gmail.com>
2019-05-29 19:42:25 +0600

    delete data using one-to-one relationship

---
+++ src/com/hibernate/one/to/one/mapping/DeleteInstructorDemo.java
@@ -0,0 +1,41 @@
+package com.hibernate.one.to.one.mapping;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.mapping.entity.Instructor;
+import com.hibernate.mapping.entity.InstructorDetailClass;
+
+public class DeleteInstructorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetailClass.class)
+				.buildSessionFactory();
+		
+		Session session = sessionFactory.getCurrentSession();
+
+		try {
+
+			session.beginTransaction();
+
+			int instructorId = 1;
+			//its delete id from both table Instructor && InstructorDetailClass
+			Instructor instructor = session.get(Instructor.class, instructorId);
+			if (instructor != null) {
+				session.delete(instructor);
+			}else {
+				System.out.println("instructor not found");
+			}
+
+			session.getTransaction().commit();
+		} finally {
+			// TODO: handle finally clause
+			sessionFactory.close();
+		}
+
+	}
+
+}
